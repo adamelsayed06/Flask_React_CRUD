f2ac31ffea2d10b0fb9a3c58f8b6dce14324baf2
adamelsayed06 <adame68@nycstudents.net>
2025-02-11 17:27:21 -0500

    fetchcontacts method

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetchContacts();
+  }, []); //empty array means it only runs once
+
+  const fetchContacts = async () =>{ //async b/c needs to wait to grab info from backend
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json(); //converts response to json
+    setContacts(data.contacts);
+    console.log(data);
+  } 
 
   return (
     <>
